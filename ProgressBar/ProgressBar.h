ec54c9397f5e125c68d6e4838837fcdb0497f34c
ahmedtalaatshaker <ahmed.talaat2021@yahoo.com>
2021-05-06 16:26:18 +0200

    Initial Commit

---
+++ ProgressBar/ProgressBar.h
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.h
+//  ProgressBar
+//
+//  Created by ahmed talaat on 06/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProgressBar.
+FOUNDATION_EXPORT double ProgressBarVersionNumber;
+
+//! Project version string for ProgressBar.
+FOUNDATION_EXPORT const unsigned char ProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressBar/PublicHeader.h>
+
+
